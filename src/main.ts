88e015367c41269c2916fb7d78fd823c3d7573e9
Dumilin V.V. <dumilinvadim@gmail.com>
2025-01-20 23:25:22 +0300

    stage

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-import { bootstrapApplication,provideProtractorTestingSupport } from '@angular/platform-browser';
+import {
+  bootstrapApplication,
+  provideProtractorTestingSupport,
+} from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent,
-    {providers: [provideProtractorTestingSupport()]})
-  .catch(err => console.error(err));
-
+bootstrapApplication(AppComponent, {
+  providers: [provideProtractorTestingSupport()],
+}).catch((err) => console.error(err));
